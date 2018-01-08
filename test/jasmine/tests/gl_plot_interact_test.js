c460f8bfb746b8dbfb2f863fbc79b7eb61a88582
Etienne Tétreault-Pinard <etienne@plot.ly>
2018-01-08 13:43:44 -0500

    trying

--- test/jasmine/tests/gl_plot_interact_test.js
+++ test/jasmine/tests/gl_plot_interact_test.js
@@ -943,7 +943,7 @@ describe('Test gl2d plots', function() {
         .then(done);
     });
 
-    it('should be able to toggle visibility', function(done) {
+    it('@noCI should be able to toggle visibility', function(done) {
         var _mock = Lib.extendDeep({}, mock);
 
         _mock.data[0].line.width = 4;
